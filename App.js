508e8d781ecc9cf27b52d45407bef3da9a5c6196
Weston Carpenter <wc.cc96@gmail.com>
2019-12-04 19:05:36 -0800

    set up navigator

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo';
 
+
+import MealsNavigator from './navigation/MealsNavigator';
+
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
@@ -19,15 +22,12 @@ export default function App() {
       startAsync={fetchFonts}
       onFinish={() => {
         setFontLoaded(true)
-      }
-      }
+      }}
     />
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MealsNavigator/>
   );
 }
 
