046b7aa06e3f47224c219a50a70e2e46dc3bac98
Weston Carpenter <wc.cc96@gmail.com>
2020-01-03 17:03:15 -0800

    added tab navigator for favorites

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -18,7 +18,8 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: 150
+        height: 150,
+        borderRadius: 10,
       },
       container: {
           flex: 1,
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,12 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { Platform } from "react-native";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -25,4 +26,9 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
