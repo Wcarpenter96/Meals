9077b05a613cda55d8bc56721812443063a86bd6
Weston Carpenter <wc.cc96@gmail.com>
2020-01-03 16:45:07 -0800

    favorite button added on header

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/dummy-data";
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
-
-    const mealId = props.navigation.getParam("mealId");
+  const mealId = props.navigation.getParam("mealId");
 
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
@@ -22,11 +23,22 @@ const MealDetailScreen = props => {
   );
 };
 
-MealDetailScreen.navigationOptions = (navigationData) => {
+MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam("mealId");
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-      headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Marked as Favorite!");
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 
