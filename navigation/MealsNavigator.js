a2f8aa5b16357b1b2a1ed0de5544c5061db5c667
Weston Carpenter <wc.cc96@gmail.com>
2020-01-08 10:32:47 -0800

    refactored meal list

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
-import FavoritesScreen from '../screens/FavoritesScreen';
+import FavoritesScreen from "../screens/FavoritesScreen";
+
+const defaultStackNavOptions = {
+  headerStyle: {
+    backgroundColor: "white"
+  },
+  headerTintColor: Colors.primaryColor
+};
 
 const MealsNavigator = createStackNavigator(
   {
@@ -18,31 +25,44 @@ const MealsNavigator = createStackNavigator(
     MealDetail: MealDetailScreen
   },
   {
-    //   initialRouteName
-    mode: 'modal',
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: 'white'
-      },
-      headerTintColor: Colors.primaryColor
-    }
+    defaultNavigationOptions: defaultStackNavOptions
   }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-    Meals: {screen: MealsNavigator, navigationOptions: {
-      tabBarIcon: (tabInfo) => {
-        return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor}/>;
+const FavNavigator = createStackNavigator(
+  {
+    Favorites: FavoritesScreen,
+    MealDetail: MealDetailScreen
+  },
+  {
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+);
+
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: tabInfo => {
+        return (
+          <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+        );
       }
-    }},
-    Favorites: {screen: FavoritesScreen, navigationOptions: {
+    }
+  },
+  Favorites: {
+    screen: FavNavigator,
+    navigationOptions: {
       // use tabBarLabel to override label
-      tabBarIcon: (tabInfo) => {
-        return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor}/>;
+      tabBarIcon: tabInfo => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
       }
-    }}
-}, {
-  tabBarOptions:{
+    }
+  }
+};
+
+const MealsFavTabNavigator = createBottomTabNavigator(tabScreenConfig, {
+  tabBarOptions: {
     activeTintColor: Colors.accentColor
   }
 });
